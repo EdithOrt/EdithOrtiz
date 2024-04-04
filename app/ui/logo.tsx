110da7ac606c5d1612bace82287db2c1722f912d
Edith <edith@toroto.mx>
2024-04-04 10:46:39 -0600

    [FIX] image component

--- app/ui/logo.tsx
+++ app/ui/logo.tsx
@@ -1,11 +1,16 @@
 import logo from '@/public/logo-eoc.svg'
 import styles from '@/app/ui/styles/home.module.scss'
 
+import Image from 'next/image'
+
 const Logo = () => {
   return (
-    <img
-      src={logo.src}
+    <Image
+      src={logo}
       className={styles.logo}
+      width={500}
+      height={500}
+      alt='Edith Ortiz Logo'
     />
   )
 }
