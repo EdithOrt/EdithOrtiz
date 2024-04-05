0a7d29a2cc0b44d3ea347fba4c0e8f8b3e36d0a1
Edith <edith@toroto.mx>
2024-04-05 15:56:53 -0600

    [UPDATE] add input forms

--- app/ui/contactForm.tsx
+++ app/ui/contactForm.tsx
@@ -1,9 +1,22 @@
 import styles from '@/app/ui/styles/contact.module.scss'
+import Title from './title'
 
 const ContactForm = () => {
   return (
     <div className={styles.main}>
-      <p>Contact Form</p>
+      <Title text='TRABAJEMOS JUNTOS(AS)' />
+
+      <form>
+        <input
+          type='text'
+          placeholder='Name'
+        />
+        <input
+          type='email'
+          placeholder='Email'
+        />
+        <textarea placeholder='Enter your message here' />
+      </form>
     </div>
   )
 }
