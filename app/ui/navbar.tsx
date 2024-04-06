9660823286578ac24ba75663eb155eefa17ddfc3
Edith <edith@toroto.mx>
2024-04-05 18:22:38 -0600

    [UPDATE] scroll buttons navbar functionality

--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -1,9 +1,21 @@
+'use client'
+
 import ButtonText from './buttons/buttonText'
 import ButtonIcon from './buttons/buttonIcon'
 
 import styles from '@/app/ui/styles/navbar.module.scss'
 
 export const Navbar = () => {
+  const handleClick = (id: string) => {
+    const targetElement =
+      document.getElementById(id)
+
+    // Scroll to the target element
+    targetElement?.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  }
   return (
     <nav className={styles.main}>
       {/* <div className={styles.home}>
@@ -11,15 +23,33 @@ export const Navbar = () => {
       </div> */}
 
       <div className={styles.containerButtons}>
-        <ButtonIcon />
-        <ButtonIcon />
+        <ButtonIcon
+          onClick={handleClick}
+          id='home'
+        />
+        <ButtonIcon
+          onClick={handleClick}
+          id='home'
+        />
       </div>
 
       <div className={styles.containerButtons}>
-        <ButtonIcon />
-        <ButtonIcon />
-        <ButtonIcon />
-        <ButtonIcon />
+        <ButtonIcon
+          onClick={handleClick}
+          id='projects'
+        />
+        <ButtonIcon
+          onClick={handleClick}
+          id='skills'
+        />
+        <ButtonIcon
+          onClick={handleClick}
+          id='aboutMe'
+        />
+        <ButtonIcon
+          onClick={handleClick}
+          id='contact'
+        />
       </div>
     </nav>
   )
