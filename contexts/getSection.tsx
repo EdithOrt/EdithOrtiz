a0a3b9581b46ec0adec7246656baed09c94967b1
Edith <edith@toroto.mx>
2024-04-15 09:27:46 -0600

    [FIX] scroll bottom or up correctly

--- contexts/getSection.tsx
+++ contexts/getSection.tsx
@@ -2,12 +2,16 @@
 
 import React, {
   createContext,
-  useState
+  useState,
+  useRef,
+  useEffect
 } from 'react'
 
 const GetSectionContext = createContext({
   section: 'home',
-  updateSection: (id: string) => {}
+  updateSection: (id: string) => {},
+  updateFlow: (newFlow: string) => {},
+  flow: { current: 'start' }
 })
 
 const GetSectionProvider: React.FC<{
@@ -16,13 +20,29 @@ const GetSectionProvider: React.FC<{
   const [section, setSection] =
     useState<string>('home')
 
+  const flow = useRef<string>('start')
+
   const updateSection = (id: string) => {
     setSection(id)
   }
 
+  const updateFlow = (newFlow: string) => {
+    flow.current = newFlow
+  }
+
+  useEffect(() => {
+    if (section === 'home') {
+      updateFlow('start')
+    } else if (section === 'contact') {
+      updateFlow('end')
+    }
+  }, [section])
+
   const data = {
     section,
-    updateSection
+    updateSection,
+    updateFlow,
+    flow
   }
   return (
     <GetSectionContext.Provider value={data}>
